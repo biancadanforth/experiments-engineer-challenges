e44b1c966ed370230bcdb12ee9539ad2e1dd2c48
Bianca Danforth <biancadanforth@gmail.com>
2018-08-13 20:38:41 -0700

    Code Testing challenge

--- code-testing/code/api.js
+++ code-testing/code/api.js
@@ -1,12 +1,35 @@
+/**
+ * Squares each element in array 'a' and returns the sum of all of the squares.
+ * Throws an error if the argument is not an array, or if any of the elements
+ * in the array are not of type 'number'. Returns 0 if 'a' is an empty array.
+ * 
+ * @param {Array.number} a - An array of numbers
+ * @return {number} - The sum of the squares of the elements in 'a'
+ */
+function sumOfSquares(arrA) {
+  // check that it's an array
+  if(!(arrA instanceof Array)) {
+    throw new Error(`Argument is not an array.`);
+  }
 
+  // check if all elements in 'a' are numbers
+  for (const ele of arrA) {
+    if (typeof ele !== 'number') {
+      throw new Error(`At least one element in the array is not a number.`);
+    }
+  }
 
-// Part 1:  foo
-function foo(a) {
-  a = a || [];
-  var b = a.map(function(el) { return el * el });
-  return b.reduce(function(s, el) { return s + el }, 0);
+  // Square each element in arrA and store it at the same index in arrB
+  var arrB = arrA.map(function(el) {
+    return el * el;
+  });
+
+  // return a single value that is the sum of all the elements in arrB
+  return arrB.reduce(function(sum, el) {
+    return sum + el;
+    // sum is initialized to 0
+  }, 0);
 }
-
 
 // Part 2:  openTabsInOrder
 
@@ -23,18 +46,17 @@ async function fakeOpenUrl (url, loadingTime=3*Math.random()) {
   * @returns workDo
   */
 async function openUrlsInOrder (urlsArray) {
-  const work = [];
-  urlsArray.forEach(async (url) => {
+  const result = [];
+  // async doesn't work in forEach loops
+  for (const url of urlsArray) {
     await fakeOpenUrl(url);
-    work.push(['url',url]);
-  })
-  work.push(['done',true]);
-  return work;
+    result.push(['url', url]);
+  }
+  result.push(['done', true]);
+  return result;
 }
 
 module.exports = {
+  sumOfSquares,
   openUrlsInOrder,
-  foo
-}
-
-
+};
--- code-testing/test/api.js
+++ code-testing/test/api.js
@@ -1,19 +1,20 @@
 // assert is like assert, but with some extra functions.
 const assert = require("assert");
 const {
-  foo,
+  sumOfSquares,
   openUrlsInOrder
 } = require('../code/api.js');
 
 
-// part 1: foo
-describe('foo()', function () {
-  it("should do something", function () {
-    assert(false, "foo has no real tests");
-  })
-  it("should do something else", function () {
-    assert(false, "foo has no real tests");
-  })
+// part 1: sumOfSquares
+describe('sumOfSquares', function () {
+  it("should throw an error if the input arg is not an array", function () {
+    assert.throws(sumOfSquares, Error, "Error thrown");
+  });
+  it("should throw an error if the input array has any non-number elements", function (done) {
+    assert.throws(function () {sumOfSquares([1, 'test'])}, Error, "Error thrown");
+    done();
+  });
 });
 
 
@@ -21,17 +22,16 @@ describe('foo()', function () {
 describe('openUrlsInOrder()', function () {
   it('should open urls in order', async function () {
 
-    const urls = ['url1', 'url2', 'url3', 'url4']
+    const urls = ['url1', 'url2', 'url3', 'url4'];
     const expected = [
       ['url', 'url1'],
       ['url', 'url2'],
       ['url', 'url3'],
       ['url', 'url4'],
       ['done', true]
-    ]
+    ];
     var result = await openUrlsInOrder(urls);
 
-    // 3. ASSERT
     assert.deepEqual(result, expected);
   });
 });
